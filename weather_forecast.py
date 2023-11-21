e7241a73000b2c13aa61179c2252958c5464fc56
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-11-21 19:06:44 +0200

    weather app requirements

---
+++ weather_forecast.py
@@ -0,0 +1,7 @@
+# Let's make an application that asks for city name and 
+# returns the weather forecast for the next 5 days.
+
+# we will use the requests library to make a request to the weather API
+# first we need to install it using pip
+# Project page: https://pypi.org/project/requests/
+# pip install requests
